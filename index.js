9bf4765f7dcbed8afff355845da3850d94e5e5ac
OmGupta1719 <guptaom1719@gmail.com>
2025-02-25 00:25:16 +0530

    Start Api Trial

--- index.js
+++ index.js
@@ -6,9 +6,21 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(express.urlencoded({extended:true}));
 
+app.set("view engine", "ejs");
+
 app.listen(port,()=>{
     console.log(`App running on port ${port}`);
 })
 app.get("/",(req,res)=>{
     res.render("home.ejs");
+})
+
+//Get searched name
+app.post("/submit",async (req,res)=>{
+    console.log(req.body.songname);
+    const result = await fetch(`https://v1.nocodeapi.com/om1719/spotify/JVijqexPXnnsIxXm/search?q=${req.body.songname}`)
+    const songData = await result.json();
+    // console.log(songData.albums[0].external_urls);
+    res.render("home.ejs",{songData});
+    
 })
