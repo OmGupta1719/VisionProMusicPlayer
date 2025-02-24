52ff1f15e2923859c991eb6a172d2013e27d7df4
OmGupta1719 <guptaom1719@gmail.com>
2025-02-25 04:02:11 +0530

    Create API and Image render

--- index.js
+++ index.js
@@ -18,9 +18,22 @@ app.get("/",(req,res)=>{
 //Get searched name
 app.post("/submit",async (req,res)=>{
     console.log(req.body.songname);
-    const result = await fetch(`https://v1.nocodeapi.com/om1719/spotify/JVijqexPXnnsIxXm/search?q=${req.body.songname}`)
+    const result = await fetch(`https://v1.nocodeapi.com/om1719/spotify/JVijqexPXnnsIxXm/search?q=${req.body.songname}&type=track`);
     const songData = await result.json();
-    // console.log(songData.albums[0].external_urls);
-    res.render("home.ejs",{songData});
-    
+    const one = songData.tracks.items[0];
+    const two = songData.tracks.items[1];
+    const three = songData.tracks.items[2];
+    const four = songData.tracks.items[3];
+    const five = songData.tracks.items[4];
+    const six = songData.tracks.items[5];
+    const seven = songData.tracks.items[6];
+    res.render("home.ejs",{
+        songOne : one,
+        songTwo : two,
+        songThree : three,
+        songFour : four,
+        songFive : five,
+        songSix : six,
+        songSeven : seven
+    });
 })
